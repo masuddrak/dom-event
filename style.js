12a5d1ca25907f2ae227134ba0652cf1f677e078
masudrana <masudranaacb@gmail.com>
2022-02-08 08:05:48 +0600

    body color change events

---
+++ style.js
@@ -0,0 +1,9 @@
+function redColor(){
+    document.body.style.backgroundColor='red';
+}
+
+const greenBody=document.getElementById('greenBody');
+greenBody.onclick=green;
+function green(){
+    document.body.style.backgroundColor='green';
+}
